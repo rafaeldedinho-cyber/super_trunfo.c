aa5edcafea08a9f71a0859a592236d8e2ea7191c
rafaeldedinho-cyber <rafaeldedinho@gmail.com>
2025-09-25 14:16:13 +0000

    carta nivel intermediario

--- cartas.c
+++ cartas.c
@@ -7,6 +7,10 @@ int main () {
     float area1 = 200000;
     float pib1 = 300000;
     int pontos1 = 10;
+    float densidade1; // densidade populacional
+    float pibpc1; //pib per capta
+    densidade1 = populacao1 / area1;
+    pibpc1 = pib1 / populacao1;
 
     printf("CARTA NUMERO 1:\n"); // imprimindo as variaveis
     printf("Estado:%c\n" , estado1) ;
@@ -16,6 +20,9 @@ int main () {
     printf("Área: %.2f Km² \n" , area1);
     printf("PIB:%.2f \n" ,  pib1); // %.2f para imprimir o float com 2 casas decimais
     printf("Pontos turísticos:%d\n" , pontos1); 
+    printf("Densidade populacional: %.2f hab/Km² \n" , densidade1);
+    printf("PIB per capta: %.2f reais \n" , pibpc1);
+
 
     char estado2 = 'B'; // declarando as variaveis
     char codigo2[5] = "B02";
@@ -24,6 +31,10 @@ int main () {
     float area2 = 300000;
     float pib2 = 400000;
     int pontos2 = 20;
+    float densidade2; // densidade populacional
+    float pibpc2; //pib per capta
+    densidade2 = populacao2 / area2;
+    pibpc2 = pib2 / populacao2;
     
     printf("\n"); //pulando uma linha
     printf("CARTA NUMERO 2:\n"); // imprimindo as variaveis
@@ -34,6 +45,9 @@ int main () {
     printf("Área: %.2f Km² \n" , area2);
     printf("PIB:%.2f \n" ,  pib2); // %.2f para imprimir o float com 2 casas decimais
     printf("Pontos turísticos:%d\n" , pontos2); 
+    printf("Densidade populacional: %.2f hab/Km² \n" , densidade2);
+    printf("PIB per capta: %.2f reais \n" , pibpc2);
+
 
 
     return 0; // finalizando o programa
